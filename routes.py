ed2dd7519f232054ca9e1dcbd54e7f22d3d59e35
Timofej_Merlin <timofejmerlin@gmail.com>
2023-06-01 08:29:44 +0300

    Сделана вёрстка страницы метода поиска в ширину

--- routes.py
+++ routes.py
@@ -1,3 +1,3 @@
 from views_python import alg_crascal_page, home_page, authors_page, breadth_first_page
-
+from logic import breadth_first
 # Реализуйте какие-то дополнительные routes здесь
--- views_python/breadth_first_page.py
+++ views_python/breadth_first_page.py
@@ -3,5 +3,9 @@ from bottle import route, view
 
 @route('/breadth_first')
 @view('breadth_first')
-def home():
+def breadth_first():
     return dict()
+#@route("/gen_breadth_first_data")
+#def gen_crascal_data():
+#    """Генерация данных для решения"""
+#    return alg_crascal.generate_matrix_to_solve()
